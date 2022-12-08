b85e04ef0334bd6e8cc40b6184d6a122f9704ac9
Spencer Boggs <spencer_boggs@icloud.com>
2022-12-07 16:49:06 -0800

    Update randomChords.py

--- randomChords.py
+++ randomChords.py
@@ -2,28 +2,25 @@ import random
 
 array = ["A", "B", "C", "D", "E", "F", "G"]
 
-
 while True:
     extraNotes = ["maj7", "min7", "7", "min7b5", "dim7", "6", "6/9", "min6"]
     finalChords = []
     def random_extra_notes():
-        chord = extraNotes[random.randint(0, len(array) - 1)]
+        chord = extraNotes[random.randint(0, len(extraNotes) - 1)]
         return chord
 
     finalChords = []
     number = input("Enter the number of chords (default: 4):")
     if (number.isnumeric()):
         number = int(number)
-        if (int(number) > (len(array) - 1)):
-            print("There are only " + str(len(array)) + " chords in the array.")
-            number = (len(array) - 1)
     else:
         number = 4
 
     i = 0;
+    selected = []
     while (i < number):
-        selected = random.sample(array, number)
-        combinedChords = selected[i] + random_extra_notes()
+        selected = array[random.randint(0, len(array) - 1)]
+        combinedChords = selected + random_extra_notes()
         finalChords.append(combinedChords)
         i = i + 1
 
