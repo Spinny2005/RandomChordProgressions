36d1f82bd1e0d5a6dec63e6b27ccdfa1d72cc0b2
Spencer Boggs <spencer_boggs@icloud.com>
2022-12-12 16:43:34 -0800

    Update randomChords.py

--- randomChords.py
+++ randomChords.py
@@ -1,9 +1,10 @@
 import random
 
 array = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+print("Enter q to quit")
 
 while True:
-    extraNotes = ["maj", "min", "maj7", "min7", "7", "min7b5", "dim7", "6", "6/9", "min6"]
+    extraNotes = ["maj7", "min7", "7", "min7b5", "dim7", "6", "6/9", "min6", "9", "maj9", "min9"]
     finalChords = []
     def random_extra_notes():
         chord = extraNotes[random.randint(0, len(extraNotes) - 1)]
@@ -13,6 +14,8 @@ while True:
     number = input("Enter the number of chords (default: 4):")
     if (number.isnumeric()):
         number = int(number)
+    elif (number == 'q'):
+        break;
     else:
         number = 4
 
